7a804a4cfaa4240f65e5d17922bf68242bd1238b
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2019-12-14 23:53:00 -0500

    Adds a lovely patterned pentagon

--- sketch.js
+++ sketch.js
@@ -5,10 +5,12 @@ let y;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   p = new Planet();
-  console.log("Planet generated.");
   let gui = new dat.GUI();
-  gui.add(p, 'radius', 0, 500);
-  gui.add(p, 'stepCount', 0, 50)
+  gui.add(p, 'radiusX', 0, 500).name("Radius - X");
+  gui.add(p, 'radiusY', 0, 500).name("Radius - Y");
+  gui.add(p, 'angle', 0, 720).name("Rotation");
+  gui.add(p, 'stepCount', 1, 50).step(1).name("# of Angles");
+
 
 }
 
@@ -17,28 +19,42 @@ function windowResized() {
 }
 
 function draw() {
+  background(80, 100, 250);
   noStroke();
-  background(100);
-  makePlanet();
+
+  fill(255);
+  for (let repeatX = windowWidth / 2 - p.radiusX; repeatX < windowWidth / 2 + p.radiusX; repeatX += 10) {
+    for (let repeatY = windowHeight / 2 - p.radiusY; repeatY < windowHeight / 2 + p.radiusY; repeatY += 10) {
+      rect(repeatX, repeatY, 5, 10);
+    }
+  }
+
+
+  // Draws rectangular background
+  fill(0);
+  beginShape();
+  vertex(0, 0);
+  vertex(windowWidth, 0);
+  vertex(windowWidth, windowHeight);
+  vertex(0, windowHeight);
+
+  // Draws planet
+  beginContour();
+  for (i = 0; i < p.stepCount + 1; i++) {
+    x = p.radiusX * sin(p.angle);
+    y = p.radiusY * cos(p.angle);
+    vertex(windowWidth / 2 - x, windowHeight / 2 - y);
+    p.angle += TWO_PI / p.stepCount;
+  }
+  endContour();
+
+  endShape();
 }
 
 function Planet() {
-  this.radius = 200;
+  this.radiusX = 150;
+  this.radiusY = 150;
   this.angle = 0;
   this.stepCount = 5;
-  this.step = TWO_PI / this.stepCount;
   this.angle = 0;
-}
-
-function makePlanet() {
-  beginShape();
-  for (i = 0; i < p.stepCount + 1; i++) {
-    x = p.radius * sin(p.angle);
-    y = p.radius * cos(p.angle);
-    vertex(windowWidth / 2 - x, windowHeight / 2 - y);
-    p.angle = p.angle + p.step;
-    if (i >= p.stepCount) {
-      endShape();
-    }
-  }
 }
