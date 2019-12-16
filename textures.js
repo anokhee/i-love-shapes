4f509d5c38a046c16f5d95f55d1b34ea887ec759
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2019-12-15 21:20:34 -0500

    Adds controls for textures

--- textures.js
+++ textures.js
@@ -1,5 +1,9 @@
+let oioi;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  oioi = new Shape(10, 12);
+  createGUI();
 }
 
 function windowResized() {
@@ -9,22 +13,42 @@ function windowResized() {
 function draw() {
   background(255);
 
-  for (let repeatX = windowWidth / 2 - 200; repeatX < windowWidth / 2 + 200; repeatX += 10) {
-    for (let repeatY = windowHeight / 2 - 200; repeatY < windowHeight / 2 + 200; repeatY += 10) {
-      drawShape(repeatX, repeatY);
+  console.log(oioi.numAngles, oioi.radius);
+
+  for (let repeatX = windowWidth / 2 - 200; repeatX < windowWidth / 2 + 200; repeatX += oioi.spacing) {
+    for (let repeatY = windowHeight / 2 - 200; repeatY < windowHeight / 2 + 200; repeatY += oioi.spacing) {
+      drawShape(oioi, repeatX + random(oioi.xjitter), repeatY + random(oioi.yjitter));
     }
   }
 }
 
-function drawShape(xPos, yPos) {
-  let numAngles = 100;
+function drawShape(shape, xPos, yPos) {
   beginShape();
-  for (var i = 0; i <= numAngles; i++) {
-    let radius = 15 + random(5);
-    var x = cos(radians(i * (360) / numAngles)) * radius;
-    var y = sin(radians(i * (360) / numAngles)) * radius;
+  for (var i = 0; i <= oioi.numAngles; i++) {
+    let radius = oioi.radius + random(oioi.linejitter);
+    var x = cos(radians(i * (360) / oioi.numAngles)) * radius;
+    var y = sin(radians(i * (360) / oioi.numAngles)) * radius;
     vertex(xPos - x, yPos - y);
   }
   endShape();
   noLoop();
+}
+
+function Shape(a, r) {
+  this.numAngles = a;
+  this.radius = r;
+  this.spacing = 10;
+  this.xjitter = 0;
+  this.yjitter = 0;
+  this.linejitter = 0;
+}
+
+function createGUI() {
+  let gui = new dat.GUI();
+  gui.add(oioi, 'radius', 0, 50).name("Pattern Radius").onChange(redraw);
+  gui.add(oioi, 'numAngles', 0, 50).name("# of Angles").onChange(redraw);
+  gui.add(oioi, 'spacing', 5, 100).step(5).onChange(redraw);
+  gui.add(oioi, 'xjitter', 0, 100).step(5).onChange(redraw);
+  gui.add(oioi, 'yjitter', 0, 100).step(5).onChange(redraw);
+  gui.add(oioi, 'linejitter', 0, 100).step(5).onChange(redraw);
 }
