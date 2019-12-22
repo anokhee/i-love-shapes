98438561f99dfb2051a8d9618248fc43d1f00786
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2019-12-21 22:49:28 -0500

    Adds style controls

--- textures.js
+++ textures.js
@@ -19,6 +19,10 @@
   - Jitteriness - X: Increases the jitteriness of the X position of each shape (offsets it within a certain range)
   - Jitteriness - Y: ^^ on the Y position
   - Line Jitteriness: (See in Shape()) Increases the squiggliness of the line
+
+  To-do: 
+  - Consolidate Pattern() and Shape() classes into one Shape() class
+  - Performance optimization for Outline Shape
 */
 
 let p;
@@ -27,8 +31,9 @@ let palette;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  p = new Shape(150, 150, 0, 0, 3);
-  oioi = new Pattern(10, 12, 12, 40, 40, 0, 0, 0);
+  colorMode(RGB);
+  p = new Shape(15, 15, 0, 0, 35);
+  oioi = new Pattern(25, 5, 5, 40, 40, 0, 0, 0);
   palette = new Palette();
   createGUI();
 }
@@ -40,28 +45,30 @@ function windowResized() {
 function draw() {
   strokeWeight(palette.strokeWeight);
   background(palette.shapeFill);
-  for (let repeatX = windowWidth / 2 - p.radiusX * 2; repeatX < windowWidth / 2 + p.radiusX * 2; repeatX += oioi.spacingX) {
-    for (let repeatY = windowHeight / 2 - p.radiusY * 2; repeatY < windowHeight / 2 + p.radiusY * 2; repeatY += oioi.spacingY) {
-      drawPattern(oioi, repeatX + random(oioi.xjitter), repeatY + random(oioi.yjitter));
+  for (let repeatX = windowWidth / 2 - p.radiusX * 20; repeatX < windowWidth / 2 + p.radiusX * 20; repeatX += oioi.spacingX) {
+    for (let repeatY = windowHeight / 2 - p.radiusY * 20; repeatY < windowHeight / 2 + p.radiusY * 20; repeatY += oioi.spacingY) {
+      drawPattern(oioi, repeatX + (Math.random() * (oioi.xjitter)), repeatY + (Math.random() * (oioi.yjitter)));
     }
   }
   drawShape();
 }
 
 function drawShape() {
-  noStroke();
   beginShape();
   fill(palette.backgroundFill);
-  vertex(0, 0);
-  vertex(windowWidth, 0);
-  vertex(windowWidth, windowHeight);
-  vertex(0, windowHeight);
+  vertex(-15, -15);
+  vertex(windowWidth + 15, -15);
+  vertex(windowWidth + 15, windowHeight + 15);
+  vertex(-15, windowHeight + 15);
   beginContour();
+  strokeWeight(palette.shapeStrokeWeight);
   for (i = 0; i < p.numAngles + 1; i++) {
-    x = (p.radiusX) * sin(p.angle) + random(p.linejitter);
-    y = p.radiusY * cos(p.angle) + random(p.linejitter);
+    let radiusX = p.radiusX + (Math.random() * p.linejitter);
+    let radiusY = p.radiusY + (Math.random() * p.linejitter);
+    x = (radiusX * Math.sin(p.angle)) * 10;
+    y = (radiusY * Math.cos(p.angle)) * 10;
     vertex(windowWidth / 2 - x, windowHeight / 2 - y);
-    p.angle += TWO_PI / p.numAngles;
+    p.angle += (Math.PI * 2) / p.numAngles;
   }
   endContour();
   endShape();
@@ -69,14 +76,14 @@ function drawShape() {
 
 function drawPattern(shape, xPos, yPos) {
   beginShape();
-  noFill();
-  strokeWeight(10);
-  stroke(random(100, 255), random(100, 255), 0);
+  fill(random(palette.patternFill1), random(palette.patternFill2), random(palette.patternFill3));
+  strokeWeight(palette.patternStrokeWeight);
+  // stroke(palette.strokeColor);
   for (var i = 0; i <= oioi.numAngles; i++) {
-    let radiusX = oioi.radiusX + random(oioi.linejitter);
-    let radiusY = oioi.radiusY + random(oioi.linejitter);
-    var x = cos(radians(i * (360) / oioi.numAngles)) * radiusX;
-    var y = sin(radians(i * (360) / oioi.numAngles)) * radiusY;
+    let radiusX = oioi.radiusX + (Math.random() * (oioi.linejitter));
+    let radiusY = oioi.radiusY + Math.random() * (oioi.linejitter);
+    var x = Math.cos(radians(i * (360) / oioi.numAngles)) * radiusX;
+    var y = Math.sin(radians(i * (360) / oioi.numAngles)) * radiusY;
     vertex(xPos - x, yPos - y);
   }
   endShape();
@@ -104,27 +111,39 @@ function Shape(rx, ry, l, a, nA) {
 }
 
 function Palette() {
-  this.shapeFill = [140, 4, 4];
-  this.backgroundFill = [200, 200, 200];
-  this.strokeWeight = 3;
-  this.patternFill = [];
+  this.shapeFill = [255, 255, 255];
+  this.patternFill1 = [255, 255, 255];
+  this.patternFill2 = [255, 255, 255];
+  this.patternFill3 = [255, 255, 255];
+  this.strokeColor = 0;
+  this.backgroundFill = [255];
+  this.shapeStrokeWeight = 1;
+  this.patternStrokeWeight = 1;
 }
 
 function createGUI() {
   let gui = new dat.GUI();
   let shapeMenu = gui.addFolder('Outline Shape');
-  shapeMenu.add(p, 'radiusX', 0, 500).step(1).name("Radius - X").onChange(redraw);
-  shapeMenu.add(p, 'radiusY', 0, 500).step(1).name("Radius - Y").onChange(redraw);
+  shapeMenu.add(p, 'radiusX', 0, 50).step(1).name("Radius - X").onChange(redraw);
+  shapeMenu.add(p, 'radiusY', 0, 50).step(1).name("Radius - Y").onChange(redraw);
   shapeMenu.add(p, 'numAngles', 1, 50).step(1).name("# of Angles").onChange(redraw);
-  shapeMenu.add(p, 'linejitter', 0, 200).step(1).name("Line Jitteriness").onChange(redraw);
+  shapeMenu.add(p, 'linejitter', 0, 20).step(1).name("Line Jitteriness").onChange(redraw);
 
   let patternMenu = gui.addFolder('Pattern Controls');
-  patternMenu.add(oioi, 'radiusX', 0, 100).step(1).name('Radius').onChange(redraw);
-  patternMenu.add(oioi, 'radiusY', 0, 100).step(1).name('Radius').onChange(redraw);
-  patternMenu.add(oioi, 'spacingX', 0, 50).step(1).name('Spacing - X').onChange(redraw);
-  patternMenu.add(oioi, 'spacingY', 0, 50).step(1).name('Spacing - Y').onChange(redraw);
+  patternMenu.add(oioi, 'radiusX', 1, 100).step(1).name('Radius').onChange(redraw);
+  patternMenu.add(oioi, 'radiusY', 1, 100).step(1).name('Radius').onChange(redraw);
+  patternMenu.add(oioi, 'spacingX', 5, 100).step(1).name('Spacing - X').onChange(redraw);
+  patternMenu.add(oioi, 'spacingY', 5, 100).step(1).name('Spacing - Y').onChange(redraw);
   patternMenu.add(oioi, 'numAngles', 0, 50).step(1).name('# of Angles').onChange(redraw);
   patternMenu.add(oioi, 'xjitter', 0, 50).step(1).name('X Position - Jitteriness').onChange(redraw);
   patternMenu.add(oioi, 'yjitter', 0, 50).step(1).name('Y Position - Jitteriness').onChange(redraw);
   patternMenu.add(oioi, 'linejitter', 0, 50).step(1).name('Line Jitteriness').onChange(redraw);
+
+  let stylesMenu = gui.addFolder('Styles');
+  stylesMenu.add(palette, 'shapeStrokeWeight', 0, 15).onChange(redraw);
+  stylesMenu.add(palette, 'patternStrokeWeight', 0, 15).onChange(redraw);
+  stylesMenu.addColor(palette, 'shapeFill').onChange(redraw);
+  stylesMenu.addColor(palette, 'patternFill1').onChange(redraw);
+  stylesMenu.addColor(palette, 'patternFill2').onChange(redraw);
+  stylesMenu.addColor(palette, 'patternFill3').onChange(redraw);
 }
